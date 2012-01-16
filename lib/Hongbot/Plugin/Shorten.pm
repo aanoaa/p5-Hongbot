597df71c38ce3931bcefa05a8c2dd03672d4939e
Hyungsuk Hong <aanoaa@gmail.com>
2012-01-16 22:58:45 +0900

    fixed bug(shorten)

--- lib/Hongbot/Plugin/Shorten.pm
+++ lib/Hongbot/Plugin/Shorten.pm
@@ -1,11 +1,13 @@
 package Hongbot::Plugin::Shorten;
 use URI;
-use Moose;
 use Mojo::DOM;
 use File::Temp;
 use AnyEvent::HTTP;
 use Encode qw(decode encode_utf8);
 use WWW::Shorten 'TinyURL';
+use HTTP::Status qw(:constants :is status_message);
+
+use Moose;
 use namespace::autoclean;
 extends 'Hongbot::Plugin';
 with 'MooseX::Role::Pluggable::Plugin';
@@ -23,42 +25,63 @@ sub hear {
         next unless $uri->scheme && $uri->scheme =~ m/^http/i;
         next unless $uri->authority;
 
-        if ($uri->host =~ m/twitter/i) {
+        if ($uri->host =~ m/twitter/) {
             my $plist = $self->parent->plugin_list;
             return if ($plist && grep { /twitter/i } @$plist);
         }
 
-        if (length "$uri" > 50 && $uri->authority !~ /tinyurl|bit\.ly/) {
+        if (length $uri->as_string > 50 && $uri->authority !~ /tinyurl|bit\.ly/) {
             $shorten = URI->new(makeashorterlink($uri))
         } else {
             $shorten = $uri;
         }
 
         my $file;
-        my ($is_html, $content_type, $charset);
+        my ($is_text, $content_type, $charset);
         my $guard; $guard = http_get $uri,
             on_header => sub {
                 my ($headers) = @_;
 
-                if (!$headers->{Status} =~ m/^2/) {
-                    $self->to_channel($cl, $channel, sprintf("httpCode: %d", $headers->{Status}));
+                if ($headers->{Status} != HTTP_OK) {
+                    $self->to_channel(
+                        $cl,
+                        $channel,
+                        sprintf(
+                            "[%s] - %s",
+                            status_message($headers->{Status}),
+                            $shorten
+                        )
+                    );
                     return;
                 }
 
                 $content_type = $headers->{'content-type'};
-                $is_html = 1 if $content_type =~ m/html?/i;
+                if ($content_type =~ m/image/i) {
+                    $self->to_channel(
+                        $cl,
+                        $channel,
+                        sprintf(
+                            "[%s] - %s",
+                            $content_type,
+                            $shorten
+                        )
+                    );
+                    return;
+                }
+
+                $is_text = $content_type =~ m/text/i;
+                return unless $is_text;
+
                 ($charset) = $content_type =~ m{charset\s*=\s*(\w+)}i;
+                return 1;
             },
             on_body => sub {
-                return unless $is_html;
-
                 $file ||= File::Temp->new(UNLINK => 1);
                 print $file $_[0];
                 return 1;
             },
             sub {
                 undef $guard;
-                $self->to_channel($cl, $channel, "[$content_type] - $shorten") unless $is_html;
                 return unless $file;
                 seek($file, 0, 0);
                 my $unknown = do { local $/; <$file> };
--- t/shorten.t
+++ t/shorten.t
@@ -15,6 +15,7 @@ my $utf8 = 'http://search.cpan.org/~tempire/Mojolicious-2.41/lib/Mojo/DOM.pm';
 #my $euckr = 'http://news.mk.co.kr/v2/view.php?sc=30500003&cm=%EC%82%AC%EC%84%A4&year=2012&no=1386&selFlag=&relatedcode=&wonNo=&sID=300';
 my $euckr = 'http://review.auction.co.kr/Feedback/FeedbackView.aspx?orderNo=656350315&category=09180100&itemNo=A562360174';
 #my $euckr = $utf8;
+my $image = "https://secure.gravatar.com/avatar/6e828df5d001a64887e4060cad244029?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png";
 my @args = ('#hongbot', 'hshong', $utf8);
 
 my $robot = Hongbot->new;
@@ -36,6 +37,11 @@ $robot->condvar->begin;
 $greet->hear($client, @args);
 pop @args;
 push @args, $euckr;
+$robot->condvar->begin;
+$greet->hear($client, @args);
+
+pop @args;
+push @args, $image;
 $robot->condvar->begin;
 $greet->hear($client, @args);
 $robot->condvar->recv;
