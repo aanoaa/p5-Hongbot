14483f5ac760c9f97091b0aa0f3b018827212254
Hyungsuk Hong <aanoaa@gmail.com>
2012-03-19 18:30:07 +0900

    oops charset

--- lib/Hongbot/Plugin/Shorten.pm
+++ lib/Hongbot/Plugin/Shorten.pm
@@ -87,6 +87,7 @@ sub hear {
                 my $unknown = do { local $/; <$file> };
                 ($charset) = $unknown =~ m{charset=['"]?([^'"]+)['"]}i unless $charset;
                 $charset =~ s{\s*/>.*}{}s;
+                $charset =~ s{;}{}s;
                 $charset //= 'utf8';
                 $charset = 'euckr' if $charset =~ m/^ks/i; # ks_c_5601-1987
                 $charset = lc $charset;
