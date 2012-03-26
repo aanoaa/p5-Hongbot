985cae033e0b002c873287c0941083947d11f185
Hyungsuk Hong <aanoaa@gmail.com>
2012-03-26 15:50:35 +0900

    improve detect charset on HTML5

--- lib/Hongbot/Plugin/Shorten.pm
+++ lib/Hongbot/Plugin/Shorten.pm
@@ -86,9 +86,8 @@ sub hear {
                 seek($file, 0, 0);
                 my $unknown = do { local $/; <$file> };
                 ($charset) = $unknown =~ m{charset=['"]?([^'"]+)['"]}i unless $charset;
-                $charset =~ s{\s*/>.*}{}s;
-                $charset =~ s{;}{}s;
                 $charset //= 'utf8';
+                $charset = 'utf8' if $charset =~ m/utf/i; # ks_c_5601-1987
                 $charset = 'euckr' if $charset =~ m/^ks/i; # ks_c_5601-1987
                 $charset = lc $charset;
                 my $data = decode($charset, $unknown);
